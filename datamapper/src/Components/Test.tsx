fb0c6a7224524ac59de710d65ba98679c35c7acd
Vithursan1003 <thevendranvithursan@gmail.com>
2023-02-28 16:38:26 +0530

    New Additions

--- datamapper/src/Components/Test.tsx
+++ datamapper/src/Components/Test.tsx
@@ -8,13 +8,12 @@ export default () => {
 	const engine = createEngine();
 	const model = new DiagramModel();
 
-
 	const node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
-	node1.addOutPort('Out');
+	node1.addOutPort('Input');
 	node1.setPosition(100, 100);
 
 	const node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
-	node2.addInPort('In');
+	node2.addInPort('Output');
 	node2.setPosition(400, 100);
 
 	model.addAll(node1, node2);
