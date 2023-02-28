af569984074b280b644b86f41d3db80faf8a89d1
hanajiffry99 <hanajiffryscg@gmail.com>
2023-02-28 23:34:36 +0530

    File Upload UI

--- datamapper/src/Components/FileUpload/UploadModel.tsx
+++ datamapper/src/Components/FileUpload/UploadModel.tsx
@@ -1,8 +1,45 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import UploadForm from './UploadForm';
+import Modal from 'react-bootstrap/Modal';
+import { DataUsageOutlined } from '@mui/icons-material';
 
 const UploadModel = () => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const headerStyle = {
+    height: '40px',
+    color: 'white',
+    backgroundColor: '#187bcd',
+    fontFamily: 'Asap',
+    fontWeight: '700',
+    fontSize: '14px',
+    paddingLeft: '30px'
+  }
+
+
   return (
-    <div>UploadModel</div>
+    <>
+      <Button variant='contained' onClick={handleClickOpen} >Open Modal</Button>
+      <Modal show={open} onHide={handleClose} centered >
+        <Modal.Header closeButton closeVariant='white' style={headerStyle}>
+          <DataUsageOutlined fontSize='small'/>
+          <div style={{ paddingLeft: '15px' }}>Load Input</div>
+        </Modal.Header>
+        <Modal.Body style={{ fontSize: '11px' }}>
+          <UploadForm />
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
 
