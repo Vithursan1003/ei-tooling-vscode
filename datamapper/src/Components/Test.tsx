a6614e1df3abb176e49845cd7c208e2307ad0d07
Vithursan1003 <thevendranvithursan@gmail.com>
2023-02-28 18:05:50 +0530

    Did Some modifications

--- datamapper/src/Components/Test.tsx
+++ datamapper/src/Components/Test.tsx
@@ -1,31 +1,27 @@
-import * as React from 'react';
-import createEngine, { DiagramModel, DefaultNodeModel } from '@projectstorm/react-diagrams';
+import createEngine, { DiagramModel, DefaultNodeModel} from '@projectstorm/react-diagrams';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
-import { DefaultState } from './DefaultState';
 import './test.css';
+import UploadForm  from './FileUpload/UploadForm';
 
 export default () => {
 	const engine = createEngine();
 	const model = new DiagramModel();
 
-	const node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
-	node1.addOutPort('Input');
+	const node1 = new DefaultNodeModel('Input', 'rgb(0,192,255)');
+	node1.addOutPort(<UploadForm />);
 	node1.setPosition(100, 100);
 
-	const node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
-	node2.addInPort('Output');
+	const node2 = new DefaultNodeModel('Output', 'rgb(192,255,0)');
+	node2.addInPort(<UploadForm />);
 	node2.setPosition(400, 100);
 
 	model.addAll(node1, node2);
 
 	engine.setModel(model);
 
-	// Use this custom "DefaultState" instead of the actual default state we get with the engine
-	engine.getStateMachine().pushState(new DefaultState());
-
 	return (
-		<>
-			<CanvasWidget className="canvas" engine={engine} />
-		</>
+		<div className='container'>
+			<CanvasWidget  className="canvas" engine={engine} />
+		</div>
 	);
 };
