ed90eb99462d11f7f05e9d97b26caeb077b04d97
Vithursan1003 <thevendranvithursan@gmail.com>
2023-03-04 22:38:44 +0530

    Modified a bit

--- datamapper-server/index.js
+++ datamapper-server/index.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
-const fs = require ('fs');
+const fs = require('fs');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const storage = multer.diskStorage({
-    destination : (req,file,cb) =>{
-        cb(null,'uploads/');
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
     },
-    filename : (req,file,cb) =>{
-        cb(null,`${file.originalname}`)
+    filename: (req, file, cb) => {
+        cb(null, `${file.originalname}`)
     },
 });
-const upload = multer({ storage});
+const upload = multer({ storage });
 
-app.post('/input/upload',upload.single('file'),(req,res)=>{
+app.post('/input/upload', upload.single('file'), (req, res) => {
+
+        fs.rename(req.file.path, req.file.path.replace(req.file.originalname, req.body.filename), (err) => {
+            if (err) {
+              return res.json({error: err.message});
+            }
+            res.json({success: true, message: `File ${req.body.filename} uploaded successfully`});
+          });
     
-    try {
-        const file = req.file;
-        fs.renameSync(req.file.path,req.file.path.replace(req.file.originalname , req.body.filename));
-        res.status(200).json({
-            status : 'success',
-            message : `${req.body.filename}`
-        });
-    } catch (error) {
-        res.json({
-            error
-        });
-    }
 });
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("server running");
 })
