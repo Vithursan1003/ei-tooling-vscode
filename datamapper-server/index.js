967255b420cf119cd5c818361a8307ecfc56079f
Vithursan1003 <thevendranvithursan@gmail.com>
2023-03-05 18:56:25 +0530

    Included codes to read a file

--- datamapper-server/index.js
+++ datamapper-server/index.js
@@ -1,33 +1,85 @@
+// const express = require('express');
+// const multer = require('multer');
+// const cors = require('cors');
+// const fs = require('fs');
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${file.originalname}`)
+//     },
+// });
+// const upload = multer({ storage });
+
+
+
+// app.post('/input/upload', upload.single('file'), (req, res) => {
+
+//         const req.file = req.file;
+
+//         // fs.rename(req.file.path, req.file.path.replace(req.file.originalname, req.body.filename), (err) => {
+//         //     if (err) {
+//         //       return res.json({error: err.message});
+//         //     }  
+//         //     fs.readFile(req.file, 'utf8', (err, data) => {
+//         //       if (err) throw err;
+//         //       console.log(data);
+//         //     }); 
+//         //     res.json({success: true, message: `File ${req.body.filename} uploaded successfully`});
+//         //   });
+//         fs.readFile(req.file, 'utf8', (err, data) => {
+//           if (err) throw err;
+        
+//           // Do something with the file contents
+//           console.log(data);
+//           console.log("File read successfully");
+//         });
+// });
+
+// app.listen(5000, () => {
+//     console.log("server running");
+// })
+
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors');
 const fs = require('fs');
-
 const app = express();
-app.use(cors());
-app.use(express.json());
+const mime = require('mime');
 
+// Define storage for the uploaded files
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${file.originalname}`)
-    },
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
 });
-const upload = multer({ storage });
 
+// Create multer middleware
+const upload = multer({ storage: storage });
+
+// Route for handling file upload and read
 app.post('/input/upload', upload.single('file'), (req, res) => {
+  const filePath = req.file.path;
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) throw err;
+    const fileType = mime.getType(req.file.path);
 
-        fs.rename(req.file.path, req.file.path.replace(req.file.originalname, req.body.filename), (err) => {
-            if (err) {
-              return res.json({error: err.message});
-            }
-            res.json({success: true, message: `File ${req.body.filename} uploaded successfully`});
-          });
-    
+    fs.rename(req.file.path, req.file.path.replace(req.file.originalname, req.body.filename), (err) => { (err) => {
+      if (err) throw err;  
+  }});
+    res.send('File uploaded and read successfully');
+  });
 });
 
+// Start server
 app.listen(5000, () => {
-    console.log("server running");
-})
+  console.log('Server started on port 5000');
+});
