32364c2a6be341f86e8b0e9943fceffcd17a7b57
Vithursan1003 <thevendranvithursan@gmail.com>
2023-03-16 11:36:17 +0530

    Added on link changes

--- datamapper/datamapper-frontend/src/Components/Test.tsx
+++ datamapper/datamapper-frontend/src/Components/Test.tsx
@@ -1,6 +1,9 @@
-import createEngine, {  DiagramModel, DefaultNodeModel, DefaultPortModel, PortModelAlignment, LabelModel, PortModel, PortWidget } from '@projectstorm/react-diagrams';
+import createEngine, { DiagramModel, DefaultNodeModel, DefaultPortModel, BaseEvent } from '@projectstorm/react-diagrams';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
 import './test.css';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap/lib/InputGroup';
+import MyButton from './Button';
 
 export default function DataMapper() {
 	//creating initial engine for the drag and drop UI
@@ -10,7 +13,19 @@ export default function DataMapper() {
 	//setting the colour of the nodes
 	const str = 'rgb(255,255,255)';
 
-	var colour = 'rgb(255,255,255)';
+	const link: any[] = [];
+
+	const [myArray, setMyArray] = useState<string[]>([]);
+
+	// Add items to the array
+	function addItem(item: string) {
+		setMyArray((prevArray: any) => [...prevArray, item]);
+	}
+
+	const handleClick = () => {
+		console.log(link);	
+	};
+
 	var node1 = new DefaultNodeModel("Input", str);
 	node1.addPort(new DefaultPortModel(true, "name4", "name"));
 	node1.addPort(new DefaultPortModel(false, "name5", "name"));
@@ -18,10 +33,11 @@ export default function DataMapper() {
 
 	node1.setPosition(100, 100);
 
-	var node2 = new DefaultNodeModel('Output', colour);
+	var node2 = new DefaultNodeModel('Output', str);
 	node2.addPort(new DefaultPortModel(true, 'name1', 'name'));
 	node2.addPort(new DefaultPortModel(true, 'name2', 'name'));
 	node2.addPort(new DefaultPortModel(true, 'name3', 'name'));
+	//console.log(port1.getName());
 
 	node2.setPosition(400, 100);
 
@@ -29,25 +45,38 @@ export default function DataMapper() {
 	engine.setModel(model);
 	//console.log(model.getLinks());
 
+	function timeout(delay: number) {
+		return new Promise(res => setTimeout(res, delay));
+	}
+
+	model.registerListener({
+		linksUpdated: async (event: BaseEvent) => {
+			var data = event.link;
+			await timeout(3000); //for 1 sec delay
+			console.log(data);
+			//addItem(data.sourcePort.getName().toString());
+			//link.push( data.sourcePort.getName(), data.options.id );
+			const newLink = {
+				sourcePort: {
+				  nodeId: data.sourcePort.parent.options.name,
+				  portId: data.sourcePort.getName(),
+				},
+				targetPort: {
+				  nodeId: data.targetPort.parent.options.name,
+				  portId: data.targetPort.getName(),
+				},
+				linkId: data.options.id,
+			  };
+			  link.push(newLink);
+		}
+	});
 	//const links = model.getLinks();
-	const id = engine.getModel() && engine.getModel().getID();
-
-	// model.registerListener({
-	// 	nodesUpdated: e => console.log("nodesUpdated", e),
-	// 	linksUpdated: e => console.log("linksUpdated", e),
-		
-	// 	// these are never triggered
-	// 	zoomUpdated: e => console.log("zoomUpdated", e),
-	// 	gridUpdated: e => console.log("gridUpdated", e),
-	// 	offsetUpdated: e => console.log("offsetUpdated", e),
-	// 	entityRemoved: e => console.log("entityRemoved", e),
-	// 	selectionChanged: e => console.log("selectionChanged", e)
-	// });
-
+	//const id = engine.getModel() && engine.getModel().getID();
 
 	return (
 		<div className="container">
 			<CanvasWidget className="canvas" engine={engine} />
+			<MyButton onClick={handleClick} style={{ position: 'absolute', top: 10, left: 10 }} />
 		</div>
 	);
 };
